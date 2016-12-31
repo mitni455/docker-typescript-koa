f23b5708f7e2b414bd8e72de268d7d8d5a632199
Jordan Schroter <jordan.schroter@gmail.com>
2016-12-30 20:12:22 -0500

    another deploy test

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ const app = new Koa()
 const router = new Router()
 
 router.get('/', function * (next) {
-  this.body = 'hello, world3.'
+  this.body = 'hello, world.'
 })
 
 app.use(router.routes())
